741b5c64b654efac7e4b105cb4e5f5a549b723e2
DerBaumann <natur5210@gmail.com>
2025-11-19 21:30:59 +0100

    Optimize DB folder structure

--- sql/queries/items.sql
+++ sql/queries/items.sql
@@ -1,21 +1,3 @@
--- name: CreateUser :one
-INSERT INTO users (username, password, email)
-VALUES ($1, $2, $3)
-RETURNING *;
-
--- name: GetUserByName :one
-SELECT * FROM users
-WHERE username = $1 OR email = $1
-LIMIT 1;
-
--- name: ListUsers :many
-SELECT id, username, email FROM users;
-
--- name: GetUserByID :one
-SELECT * FROM users
-WHERE id = $1
-LIMIT 1;
-
 -- name: ListItems :many
 SELECT * FROM items
 WHERE user_id = $1
@@ -46,3 +28,7 @@ SET
 WHERE id = $4
 RETURNING *;
 
+-- name: DeleteItem :one
+DELETE FROM items
+WHERE id = $1
+RETURNING *;
---
+++ sql/queries/users.sql
@@ -0,0 +1,18 @@
+-- name: CreateUser :one
+INSERT INTO users (username, password, email)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetUserByName :one
+SELECT * FROM users
+WHERE username = $1 OR email = $1
+LIMIT 1;
+
+-- name: ListUsers :many
+SELECT id, username, email FROM users;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1
+LIMIT 1;
+
